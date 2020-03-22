f7f5bf9c4f7c25ea2e963773273b7508d98e291e
Elender <elendergallas@infraero.gov.br>
2020-03-21 21:38:35 -0300

    Update campob.js

--- js/campob.js
+++ js/campob.js
@@ -1,13 +1,18 @@
 function DecodificarCampoB(){     
    var campoB = document.getElementById("campoB").value;
    console.log(campoB);
-   ano = "20" + campoB[0] + campoB[1];
-   mes = campoB[2] + campoB[3];
-   dia = campoB[4] + campoB[5];
-   hora = campoB[6] + campoB[7]
+   ano = "20" + campoB[0] + campoB[1] + " ";
+   mes = campoB[2] + campoB[3] + "/";
+   dia = campoB[4] + campoB[5] + "/";
+   hora = "Hora: " + campoB[6] + campoB[7] + ":";
    minuto = campoB[8] + campoB[9]
+   console.log("Data de Início: "+dia+mes+ano+hora+minuto);
 
+   var p = document.createElement("p");            // Create a <p> node        
+   var t = document.createTextNode("Data de Início: "+dia+mes+ano+hora+minuto );       // Create a text node        
+   p.appendChild(t);                              // Append the textnode to <li>
+   document.getElementById("B").appendChild(p);    // Append another <p> to <p> with id="Q"   
    
 
-   //
+   //1407011041
 }
